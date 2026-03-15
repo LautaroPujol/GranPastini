57b58c9d4ed1c9222d73751bfa6079ecbf764895
LautaroPujol <lautaropujoll99@gmail.com>
2026-03-15 10:36:24 -0300

    Arreglando errores de firebase

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,21 +1,19 @@
-// src/services/firebaseConfig.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  // ⚠️ ACÁ PEGÁ TUS CREDENCIALES DE FIREBASE (Las que ya tenías)
-  apiKey: "AIzaSyDqpTavZxhwQHf5WVg_J6_M26s4wiDSQ0g",
-  authDomain: "granpastini-27f3f.firebaseapp.com",
-  projectId: "granpastini-27f3f",
-  storageBucket: "granpastini-27f3f.firebasestorage.app",
-  messagingSenderId: "781200814070",
-  appId: "1:781200814070:web:48ff1205d3bf9d4f66a1d7"
+
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
-// Inicializamos la App
+
 const app = initializeApp(firebaseConfig);
 
-// Exportamos solo lo que vamos a usar: Auth (Login) y DB (Base de datos)
 export const auth = getAuth(app);
 export const db = getFirestore(app);
